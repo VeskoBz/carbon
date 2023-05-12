30447379f7fbe276716cda4b6aa5e2cb36807f00
kicharr <99511093+kicharr@users.noreply.github.com>
2023-05-12 09:04:11 +0300

    last changes

--- js/weather.js
+++ js/weather.js
@@ -1,46 +1,160 @@
 // request to weather
-//55.640721, 49.309943 
-const convertTemp = (celvin) => celvin - 273.15;
-const convertPressuare = (megaPascal) => megaPascal / 1.333
+//55.640721, 49.309943
+const convertTemp = (celvin) => Math.floor(celvin - 273.15);
+const convertPressure = (megaPascal) => Math.floor(megaPascal / 1.333)
 
-fetch('https://api.openweathermap.org/data/2.5/weather?&cnt=4&id=551487&appid=53cff6d53d5d0e62db130cd243ec6e32&lang=ru')
+const dataOption = {
+    day: 'numeric',
+    month: 'numeric'
+}
 
-.then(function (resp){return resp.json() })
-.then(function (data) {
-    console.log(data)
-    document.querySelector('.city-name').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = Math.round( convertTemp(data.main.temp)) + ' ' +'&deg';
-    document.querySelector('.weather').innerHTML = data.weather[0]['description'];
-    document.querySelector('.pressure').innerHTML = data.main.pressure;
+let labels = [];
 
-    let ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx, {
+const tempDataSets = []
+
+const humidityDataSets = []
+
+const pressureDataSets = [];
+
+const charts = [];
+
+function createChart(ctx, datasets, id) {
+    const chartIndex = charts.map(item => item.id).indexOf(id);
+    if (chartIndex !== -1) {
+        charts[chartIndex].chart.update();
+        return;
+    }
+    const chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['Январь'],
-            datasets: [{
-                label: 'Температура за день',
-                data: [convertTemp(data.main.temp), convertPressuare(data.main.pressure)],
-                borderWidth: 4
-            }]
+            labels,
+            datasets
         },
-        options: {},
+
+    });
+
+
+    charts.push({id, chart});
+
+}
+
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?appid=53cff6d53d5d0e62db130cd243ec6e32&lang=ru&lat=55.640721&lon=49.30994')
+
+
+    .then(async function (resp) {
+        const data = await resp.json();
+        const newData = [];
+        for (let i = 0; i < data.list.length; i++) {
+            let add = true;
+            if (newData.map(item => new Date(item.dt_txt).getDate()).indexOf(new Date(data.list[i].dt_txt).getDate()) !== -1) {
+                add = false;
+            }
+            if (add) {
+                newData.push(data.list[i])
+            }
+        }
+
+        labels = labels.length ? labels : newData.map(item => new Date(item.dt_txt).toLocaleDateString('ru-RU', dataOption));
+
+        tempDataSets.push({
+            label: `${data.city.name}`,
+            data: newData.map(item => convertTemp(item.main.temp)),
+            borderColor: '#636F19',
+            backgroundColor: '#636F19',
+            borderWidth:5,
+        });
+        humidityDataSets.push({
+            label: `${data.city.name}`,
+            data: newData.map(item => convertTemp(item.main.humidity)),
+            borderColor: '#636F19',
+            backgroundColor: '#636F19',
+            borderWidth:5,
+        });
+        pressureDataSets.push({
+            label: `${data.city.name}`,
+            data: newData.map(item => convertTemp(item.main.pressure)),
+            borderColor: '#636F19',
+            backgroundColor: '#636F19',
+            borderWidth:5,
+        });
+
+        createChart(document.getElementById('temp').getContext('2d'), tempDataSets, 'temp');
+        createChart(document.getElementById('humidity').getContext('2d'), humidityDataSets, 'humidity');
+        createChart(document.getElementById('pressure').getContext('2d'), pressureDataSets, 'pressure');
+
     })
-})
-.catch(function (){
-    //catch any errors
+    .catch(function (e) {
+        console.log(e);
+        //catch any errors
+    })
+
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?appid=53cff6d53d5d0e62db130cd243ec6e32&lang=ru&lat=55.27265&lon=49.27039').then(async res => {
+    const data = await res.json();
+
+    const newData = [];
+    for (let i = 0; i < data.list.length; i++) {
+        let add = true;
+        if (newData.map(item => new Date(item.dt_txt).getDate()).indexOf(new Date(data.list[i].dt_txt).getDate()) !== -1) {
+            add = false;
+        }
+        if (add) {
+            newData.push(data.list[i])
+        }
+    }
+
+    labels = labels.length ? labels : newData.map(item => new Date(item.dt_txt).toLocaleDateString('ru-RU', dataOption));
+
+    tempDataSets.push({
+        label: `${data.city.name}`,
+        data: newData.map(item => convertTemp(item.main.temp)),
+            borderColor: '#FA7921',
+            backgroundColor: '#FA7921',
+            borderWidth:5,
+    });
+    humidityDataSets.push({
+        label: `${data.city.name}`,
+        data: newData.map(item => convertTemp(item.main.humidity)),
+            borderColor: '#FA7921',
+            backgroundColor: '#FA7921',
+            borderWidth:5,
+    });
+    pressureDataSets.push({
+        label: `${data.city.name}`,
+        data: newData.map(item => convertTemp(item.main.pressure)),
+            borderColor: '#FA7921',
+            backgroundColor: '#FA7921',
+            borderWidth:5,
+    });
+
+    createChart(document.getElementById('temp').getContext('2d'), tempDataSets, 'temp');
+    createChart(document.getElementById('humidity').getContext('2d'), humidityDataSets, 'humidity');
+    createChart(document.getElementById('pressure').getContext('2d'), pressureDataSets, 'pressure');
+
+
+}).catch((e) => {
+    console.log(e);
+});
+
+fetch('https://api.openweathermap.org/data/2.5/weather?appid=53cff6d53d5d0e62db130cd243ec6e32&lang=ru&lat=55.640721&lon=49.30994').then(async res => {
+    const data = await res.json();
+    document.getElementById('first-city-name').innerText = data.name;
+    document.getElementById('first-temp').innerText = convertTemp(data.main.temp) ;
+    document.getElementById('first-pressure').innerText = convertPressure(data.main.pressure) ;
+    document.getElementById('first-humidity').innerText = data.main.humidity;
+
 })
 
-// fetch('https://api.openweathermap.org/data/2.5/weather?id=466806&appid=53cff6d53d5d0e62db130cd243ec6e32&lang=ru')
-//
-//     .then(function (resp){return resp.json() })
-//     .then(function (data) {
-//         console.log(data)
-//         document.querySelector('.city-name-yo').innerHTML = data.name;
-//         document.querySelector('.temp-yo').innerHTML = Math.round(data.main.temp - 273) + ' ' +'&deg';
-//         document.querySelector('.weather-yo').innerHTML = data.weather[0]['description'];
-//     })
-//     .catch(function (){
-//         //catch any errors
-//     })
+fetch('https://api.openweathermap.org/data/2.5/weather?appid=53cff6d53d5d0e62db130cd243ec6e32&lang=ru&lat=55.27265&lon=49.27039').then(async res => {
+    const data = await res.json();
+    document.getElementById('second-city').innerText = data.name;
+    document.getElementById('second-temp').innerText = convertTemp(data.main.temp);
+    document.getElementById('second-pressure').innerText = convertPressure(data.main.pressure);
+    document.getElementById('second-humidity').innerText = data.main.humidity;
+})
+
+
+
+
 
